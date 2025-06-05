6ed112e7510e46428753607cd038da9678191bf9
Antón Liñares López <antonlinares03@gmail.com>
2025-06-05 07:21:03 +0200

    Implemented checkers, setters, and all posible tiles

--- CHESS.jl
+++ CHESS.jl
@@ -20,7 +20,9 @@ struct Bishop <: Piece
     team::Bool
 end
 
-struct Game
+
+
+mutable struct Game
     Win::Bool
     Team::Bool # 0 para las blancas, 1 para las negras
     Board::Matrix{Any}
@@ -31,9 +33,31 @@ end
 # -------------------------------------- MÉTODOS CLASES -----------------------------------------
 # -----------------------------------------------------------------------------------------------
 
+# --------------------------------------- GETTERS Y SETTERS --------------------------------------
 
+# Getters and Setters for Game and Piece
 function GetBoard(Game::Game)
     return Game.Board
+end
+
+function SetBoard(Game::Game, NewBoard::AbstractArray{Any,2})
+    if is_square_8(NewBoard)
+        Game.Board = NewBoard
+    else
+        error("New board is not 8x8")
+    end
+end
+
+function MovePiece(Game::Game, PosI::Vector{Int64}, PosF::Vector{Int64})
+    """
+    Changes the position of a piece in the board.
+    """
+    if isa(GetBoard(Game, PosI), Piece)
+        Game.Board[PosF[1], PosF[2]] = GetBoard(Game, PosI)
+        Game.Board[PosI[1], PosI[2]] = 0
+    else
+        error("No piece in position $PosI")
+    end
 end
 
 function GetBoard(Game::Game, pos::Vector{Int64})
@@ -54,6 +78,14 @@ end
 
 function ChangeBoard(Game::Game,NewBoard::AbstractArray{Any,2})
     Game.Board = NewBoard
+end
+
+function ChangeTeam(Game::Game)
+    Game.Team = !Game.Team
+end
+
+function ChangeTeam(Game::Game, NewTeam::Bool)
+    Game.Team = NewTeam
 end
 
 function KillPiece(Piece::Piece)
@@ -80,6 +112,9 @@ function CreateFullBoard()
     Board[1,2] = Bbishop()
     Board[1,7] = Bbishop()
 
+    Board[6,1] = Wrook()
+    Board[6,8] = Wrook()
+
     return Game(false, false, Board)
 end
 
@@ -91,12 +126,15 @@ end
 function Wrook()
     return Rook(true,false)
 end
+
 function Brook()
     return Rook(true,true)
 end
+
 function Wbishop()
     return Bishop(true,false)
 end
+
 function Bbishop()
     return Bishop(true,true)
 end
@@ -109,20 +147,22 @@ end
 
 
 function GetCoords(coords)
-    translate =  Dict('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5,'f'=>6,'g'=>7,'h'=>8,
-    '1'=>8,'2'=>7,'3'=>6,'4'=>5,'5'=>4,'6'=>3,'7'=>2,'8'=>1)
 
-    RealCoords = []
+    translate = Dict{Char, Int64}(
+    'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7, 'h' => 8,
+    '1' => 8, '2' => 7, '3' => 6, '4' => 5, '5' => 4, '6' => 3, '7' => 2, '8' => 1
+    )
+    RealCoords::Vector{Int64} = Int64[]
 
-    for coord in coords
-        
+    for coord in coords        
         if haskey(translate, coord)
             push!(RealCoords, translate[coord])
         else
             println("No encontrada la coordinada", coords)
         end
-        return RealCoords
     end
+
+    return RealCoords
 
 end
 
@@ -133,10 +173,14 @@ end
 function Visual(item::Any)
     if isa(item, Piece)
 
-        if isa(item,Rook)
+        if isa(item,Rook) && GetTeam(item)
             return "R"
-        elseif isa(item,Bishop)
+        elseif isa(item,Rook) && !GetTeam(item)
+            return "r"
+        elseif isa(item,Bishop) && GetTeam(item)
             return "B"
+        elseif isa(item,Bishop) && !GetTeam(item)
+            return "b"
         else
             return "P"
         end
@@ -169,6 +213,31 @@ function BePrinter(Game::Game)
     end
 end
 
+function BePrinter(Game::Game, PlausibleMoves::Vector{Vector{Int64}})
+    cnt::Int64 = 8
+    abec = "abcdefgh"
+
+    print("  ")
+    for letter in abec
+        print(letter, " ")
+    end
+
+    println("")
+
+    for row in 1:8
+        print(cnt, " ")
+        cnt -= 1
+        for col in 1:8
+            if any(isequal([row, col]), PlausibleMoves)
+                print("X", " ")
+            else
+                print(Visual(GetBoard(Game, [row, col])) * " ")
+            end
+        end
+        println("")
+    end
+end
+
 
 # -----------------------------------------------------------------------------------------------
 # ----------------------------------- COMPROBACIÓN LEGAL ----------------------------------------
@@ -188,21 +257,15 @@ end
 
 
 # --------------------------------- CHECKS FOR ROOK ----------------------------------------------
-
-
+"""
 function LegalRook(Game::Game,PosI::Vector{Int64},PosF::Vector{Int64})
-
     if CanMoveRook(Game::Game,PosI::Vector{Int64},PosF::Vector{Int64})
-
-
-
-
     else
         return false
     end
-
-
 end
+
+
 
 function PlausiblePosRook(Vector::Vector{Any}, Team::Bool)
     if isa(Vector[1], Rook)
@@ -214,18 +277,12 @@ function PlausiblePosRook(Vector::Vector{Any}, Team::Bool)
             end
         end
 
-
-
-
     else
         error("Element 1 from PlausiblePosRook is not a rook")
     end
-
-
 end
+
 function PlausiblePosRook()
-
-
     PlausiblePosRook(Vector, Team)
 end
 
@@ -249,6 +306,83 @@ end
 
 
 
+"""
+
+function AllPlausiblePosRook(Game::Game, PosI::Vector{Int64})
+    """
+    Returns all plausible positions for a rook given its initial position.
+    """
+    # PlausiblePositions will be a vector of Integer vectors
+
+    plausible_positions::Vector{Vector{Int64}} = Vector{Vector{Int64}}()
+
+    # Check horizontal movement
+    # We add plausible if not piece. We break if ally piece. We add and break if enemy piece
+    for col in (PosI[2]+1):8
+        # We check wether POSITION is occupied by PIECE of SAMETEAM or NOT.
+        Tile = GetBoard(Game, [PosI[1], col])
+
+        if isa(Tile, Piece) && SameTeam(Tile, Game)
+            break
+        elseif isa(Tile, Piece) && !SameTeam(Tile, Game)
+            push!(plausible_positions, [PosI[1], col])
+            break
+        end
+        push!(plausible_positions, [PosI[1], col])
+    end
+
+    for col in (PosI[2]-1):-1:1
+        Tile = GetBoard(Game, [PosI[1], col])
+        if isa(Tile, Piece) && SameTeam(Tile, Game)
+            break
+        elseif isa(Tile, Piece) && !SameTeam(Tile, Game)
+            push!(plausible_positions, [PosI[1], col])
+            break
+        end
+        push!(plausible_positions, [PosI[1], col])
+    end
+
+    # Check vertical movement
+
+    for row in (PosI[1]+1):8
+        Tile = GetBoard(Game, [row, PosI[2]])
+        if isa(Tile, Piece) == true && SameTeam(Tile, Game)
+            break
+        elseif isa(Tile, Piece) == true && !SameTeam(Tile, Game)
+            push!(plausible_positions, [row, PosI[2]])
+            break
+        end
+        push!(plausible_positions, [row, PosI[2]])
+    end
+
+    for row in (PosI[1]-1):-1:1
+        Tile = GetBoard(Game, [row, PosI[2]])
+        if isa(Tile, Piece) == true && SameTeam(Tile, Game)
+            break
+        elseif isa(Tile, Piece) == true && SameTeam(Tile, Game)
+            push!(plausible_positions, [row, PosI[2]])
+            break
+        end
+        push!(plausible_positions, [row, PosI[2]])
+    end
+
+    return plausible_positions
+end
+
+
+function AllPlausiblePos(Game::Game, PosI::Vector{Int64})
+    """
+    Returns all plausible positions for a piece given its initial position.
+    """
+    if IsaRook(Game, PosI)
+        return AllPlausiblePosRook(Game, PosI)
+    elseif IsaBishop(Game, PosI)
+        return []
+        # return AllPlausiblePosBishop(Game, PosI)
+    else
+        return []  # No plausible positions for other pieces
+    end
+end
 
 
 
@@ -256,18 +390,66 @@ end
 function LegalBishop()
 end
 # -----------------------------------------------------------------------------------------------
-# --------------------------------------- CHECKS INITIAL POS -----------------------------------
+# ----------------------------------------- CHECKS TEAMS ----------------------------------------
 # -----------------------------------------------------------------------------------------------
+# The idea fot this was to make it so we check wether the move requested is legal or not.
+
+
+function SameTeam(Piece1::Piece, Piece2::Piece)
+    """
+    Checks wether Piece1 and Piece2 belong to the same team.
+    Returns true if they do, false otherwise.
+        input:
+            Piece1: Piece object
+            Piece2: Piece object
+    """
+    return GetTeam(Piece1) == GetTeam(Piece2)
+end
+
+function SameTeam(Piece::Piece, Game::Game)
+    """
+    Checks wether Piece belongs to the same team as the Game.
+    Returns true if it does, false otherwise.
+        input:
+            Piece: Piece object
+            Game: Game object
+    """
+    return GetTeam(Piece) == GetTeam(Game)
+end
 
 function IsaPiece(Game::Game,Coords::Vector{Int64})
+    # Used by CheckInitialPosition
+    """
+    Checks wether COORDS selected are a Piece.
+    Returns true if it is, false otherwise.
+        input:
+            Game: Game object
+            Coords: Vector of Int64 with coordinates of the piece
+    """
     return isa(GetBoard(Game,Coords),Piece)
 end
 
 function CorrectTeam(Game::Game,Coords::Vector{Int64})
+    """
+    Checks wether Piece selected BELONGS to same team as PLAYER.
+    Returns true if it does, false if it doesn't.
+        input:
+            Game: Game object
+            Coords: Vector of Int64 with coordinates of the piece
+    """
+    # Used by CheckInitialPosition
     return GetTeam(GetBoard(Game, Coords)) == GetTeam(Game)
 end
 
+
 function CheckInitialPosition(Game::Game,Coords::Vector{Int64})
+    """
+    Checks wether COORDS selected are PIECE, and wether BELONGS to PLAYER TURN.
+    Returns true if both conditions are met, false otherwise.
+        input:
+            Game: Game object
+            Coords: Vector of Int64 with coordinates of the piece
+    """
     if IsaPiece(Game,Coords)
         return CorrectTeam(Game, Coords)
     else 
@@ -278,6 +460,7 @@ end
 # -----------------------------------------------------------------------------------------------
 # --------------------------------------- CHECKS FINAL POS --------------------------------------
 # -----------------------------------------------------------------------------------------------
+# The idea for this is to check wether the move requested is legal or not.
 
 function EspecificCheck(Game::Game,PosI::Vector{Int64},PosF::Vector{Int64})
     
@@ -300,28 +483,83 @@ end
 # --------------------------------------- GAME LOOP PRIMITIVO -----------------------------------
 # -----------------------------------------------------------------------------------------------
 
-
 function main()
 
-    println("workinga")
+    println("working")
 
     Game = CreateFullBoard()
-    BePrinter(Game)
-
+    
     while !GetWin(Game)
-
         BePrinter(Game)
+        println("Put the next coordinates in this way 1a would be [1,a]")
+        println("Current team is: ", GetTeam(Game) ? "Black" : "White")
 
-        println("Put the next coordinates in this way [1,a] would be 1a")
-
+        # We get the coordinates for PosI
         Fakecoordinates = readline()
-        coordinates = GetCoords(Fakecoordinates)
-        println("the coordinates you choose were $coordinates")
+        PosI = GetCoords(Fakecoordinates)
+        println("the coordinates you choose were $PosI")
+
+
+        # Now coordinates were chosen, we must only follow up if a piece gets selected.
+        if CheckInitialPosition(Game, PosI)
+            println("PIECE FROM YOUR TEAM!")
+            # Once a piece gets selected:
+            # We want to print all tiles that are plausible tiles for the piece.
+            PosibleTiles = AllPlausiblePos(Game, PosI)
+
+            println("The possible tiles for this piece are: $PosibleTiles")
+            BePrinter(Game, PosibleTiles)
+            println("Put the next coordinates in this way 1a would be [1,a]")
+
+            # Now its time to choose the final position.
+            Fakecoordinates = readline()
+            PosF = GetCoords(Fakecoordinates)
+            println("the coordinates you choose were $PosF")
+
+            # Now we check if the final position is plausible for the piece.
+            
+            if PosF in PosibleTiles
+                println("The move is plausible")
+                # We move the piece.
+                MovePiece(Game, PosI, PosF)
+            else
+                println("The move is not plausible")
+                continue
+            end
 
 
 
 
 
+
+
+
+            
+
+
+
+            # Where do we move the piece?
+
+
+
+
+
+        else
+            println("PIECE NOT FROM YOUR TEAM OR NOT A PIECE")
+            continue
+        end
+
+
+
+
+
+        # Where do we move the piece?
+
+
+
+
+
+        ChangeTeam(Game)
     end
 
 end
